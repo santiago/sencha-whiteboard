eca49db534cacb6675b186ca16128561344f4f20
Jason Cline <jason.cline@sencha.com>
2012-04-25 17:12:33 -0700

    reload chats sync button

--- touch/app/controller/Messaging.js
+++ touch/app/controller/Messaging.js
@@ -6,13 +6,18 @@ Ext.define('ioExamples.controller.Messaging', {
         control: {
             userlist: {
                 select: 'showUserMessages'
-            },     
+            },
             messagefield: {
-              action:"sendMessage"
+                action: "sendMessage"
             },
             peoplebackBtn: {
                 tap: 'doPeopleBack'
+            },
+
+            synchatstoreBtn: {
+                tap: 'syncChatStore'
             }
+
         },
 
         refs: {
@@ -21,56 +26,75 @@ Ext.define('ioExamples.controller.Messaging', {
             peoplePanel: '#peoplepanel',
             messagefield: "#messagefield",
             peoplebackBtn: 'button[action=peopleback]',
-            chatList: "#chatList"
+            chatList: "#chatList",
+            synchatstoreBtn: 'button[action=synchatstore]'
         }
     },
 
-    showUserMessages: function(list, record){
-      this.selectedUser = record;
-      
-      console.log("showUserMessages", record, this.getPeoplePanel(), this.selectedUser);
-      this.getPeoplePanel().setActiveItem(1);
-      
-      var chats = Ext.data.StoreManager.lookup('chats');
-      console.log("record.data.userID", record, record.data.id);
-      chats.clearFilter(true);
-      chats.filter("userID", record.data.id);
+    showUserMessages: function(list, record) {
+        this.selectedUser = record;
 
-      this.getPeoplePanel().getAt(0).setTitle(this.selectedUser.data.name);
-      this.getPeoplebackBtn().show();
+        console.log("showUserMessages", record, this.getPeoplePanel(), this.selectedUser);
+        this.getPeoplePanel().setActiveItem(1);
+
+        var chats = Ext.data.StoreManager.lookup('chats');
+        console.log("record.data.userID", record, record.data.id);
+        chats.clearFilter(true);
+        chats.filter("userID", record.data.id);
+
+        this.getPeoplePanel().getAt(0).setTitle(this.selectedUser.data.name);
+        this.getPeoplebackBtn().show();
     },
-    
-    sendMessage: function(msgField){
-      var message = msgField.getValue();
-       console.log("sendMessage",message);
-       var user = this.selectedUser.data.userObj;
-       var chatList = this.getChatList();
-       user.send({message:message, callback: function(){
-         console.log("sendMessage callback", arguments);
-         
-         
-         var chats = Ext.data.StoreManager.lookup('chats');
-          var record = {
-             message: message,
-             userID: user.key,
-             from: "ME",
-             date: new Date().getTime()
-           };
-           console.log("saving message", record);
-           chats.add(record);
-           setTimeout(function() {chatList.getScrollable().getScroller().scrollToEnd();}, 300);
-           chats.sync();
-           msgField.setValue("");
-       }});
+
+    sendMessage: function(msgField) {
+        var message = msgField.getValue();
+        console.log("sendMessage", message);
+        var user = this.selectedUser.data.userObj;
+        var chatList = this.getChatList();
+        var self = this;
+        user.send({
+            message: message,
+            callback: function() {
+                console.log("sendMessage callback", arguments);
+                var chats = Ext.data.StoreManager.lookup('chats');
+                var record = {
+                    message: message,
+                    userID: user.key,
+                    from: "ME",
+                    date: new Date().getTime()
+                };
+                console.log("saving message", record);
+                chats.add(record);
+                self.scrollMessages();
+                chats.sync();
+                msgField.setValue("");
+            }
+        });
     },
-    
-    doPeopleBack: function(){
-      this.getPeoplePanel().setTitle("People");
-       this.getPeoplePanel().setActiveItem(0);
-       
-       this.getUserlist().deselectAll();
-       this.getPeoplebackBtn().hide();
+
+    doPeopleBack: function() {
+        this.getPeoplePanel().setTitle("People");
+        this.getPeoplePanel().setActiveItem(0);
+
+        this.getUserlist().deselectAll();
+        this.getPeoplebackBtn().hide();
+    },
+
+    scrollMessages: function() {
+        setTimeout(function() {
+            chatList.getScrollable().getScroller().scrollToEnd();
+        },
+        300);
+    },
+
+    syncChatStore: function() {
+        var chats = Ext.data.StoreManager.lookup('chats');
+        var self = this;
+        chats.sync(function() {
+            self.scrollMessages();
+            console.log("chat sync callback", arguments);
+        });
     }
-    
-    
+
+
 });
--- touch/app/view/People.js
+++ touch/app/view/People.js
@@ -1,7 +1,7 @@
 Ext.define("ioExamples.view.People", {
     extend: 'Ext.Panel',
     xtype: "exmaplespeople",
-    requires: ["Ext.dataview.List", "Ext.form.Panel","Ext.form.FieldSet"],
+    requires: ["Ext.dataview.List", "Ext.form.Panel", "Ext.form.FieldSet"],
     config: {
         id: "peoplepanel",
         title: 'People',
@@ -13,7 +13,18 @@ Ext.define("ioExamples.view.People", {
             xtype: 'titlebar',
             title: 'People',
             items: [
-              {text: "back", action:"peopleback", hidden:"true"}
+            {
+                text: "back",
+                action: "peopleback",
+                hidden: "true"
+            },
+            {
+                action: "synchatstore",
+                align: 'right',
+                iconMask: true,
+                ui: 'plain',
+                iconCls: 'refresh'
+            }
             ]
         },
         {
@@ -28,36 +39,36 @@ Ext.define("ioExamples.view.People", {
         },
         {
             xtype: "panel",
-            layout:"fit",
-            items:[
-              {
-                  xtype: "list",
-                  id: "chatList",
-                  cls: "noline",
-                  styleHtmlContent: true,
-                  scrollable: true,
-                  scrollToTopOnRefresh: false,
-                  disableSelection: true,
-                  itemTpl: '<div class="{[values.from == "ME" ? "messageFromMe" : "messageFrom"]}">{message}</div>',
-                  emptyText: 'No Messages'
-              },
-              {
-                xtype:"toolbar",
+            layout: "fit",
+            items: [
+            {
+                xtype: "list",
+                id: "chatList",
+                cls: "noline",
+                styleHtmlContent: true,
+                scrollable: true,
+                scrollToTopOnRefresh: false,
+                disableSelection: true,
+                itemTpl: '<div class="{[values.from == "ME" ? "messageFromMe" : "messageFrom"]}">{message}</div>',
+                emptyText: 'No Messages'
+            },
+            {
+                xtype: "toolbar",
                 docked: 'bottom',
                 scrollable: "none",
-                
+
                 items: [
-                         {
-                              id: "messagefield",
-                              xtype: 'textfield',
-                              name: 'message',
-                              width: "90%",
-                              placeHolder: 'Send a message'
-                          }
-                    ]
-              
-              }
-            
+                {
+                    id: "messagefield",
+                    xtype: 'textfield',
+                    name: 'message',
+                    width: "90%",
+                    placeHolder: 'Send a message'
+                }
+                ]
+
+            }
+
             ]
         }
         ]
